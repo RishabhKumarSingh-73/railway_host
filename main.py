acb665d617ff49de5d11da9abbe32d4b46c13c8f
RishabhKumarSingh-73 <rishabhkumarsinghb@gmail.com>
2025-03-15 08:43:43 +0530

    add other assessments

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import json
 import os
+import re
 from fastapi import FastAPI
+from openai import OpenAI
 import uvicorn
 
-from llm_functions.memory import memory_assessment
+from llm_functions.memory import memory_assessment,logical_assessment,comprehension_assessment,topic_assessment
 
 app = FastAPI()
+
+api_key = os.getenv("API_KEY")
+
+print(api_key)
+
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key = api_key,
+    )
 
 @app.get("/hello")
 def hello_world():
@@ -13,15 +24,39 @@ def hello_world():
 
 @app.get("/memory")
 async def memory_assessment_endpoint():
-    response = await memory_assessment()
-    data_dict = json.loads(response)
+    response = await memory_assessment(client)
+    data_dict = json.loads(extract_json(response))
     return data_dict
 
 @app.get("/logical")
 async def logical_assessment_endpoint():
-    response = await memory_assessment()
-    data_dict = json.loads(response)
+    response = await logical_assessment(client)
+    data_dict = json.loads(extract_json(response))
     return data_dict
+
+@app.get("/comprehension")
+async def comprehension_assessment_endpoint():
+    response = await comprehension_assessment(client)
+    data_dict = json.loads(extract_json(response))
+    return data_dict
+
+@app.get("/topic/{topic}")
+async def comprehension_assessment_endpoint(topic:str):
+    response = await topic_assessment(client,topic)
+    data_dict = json.loads(extract_json(response))
+    return data_dict
+
+
+
+
+def extract_json(response: str) -> str:
+    
+    match = re.search(r'\{.*\}', response, re.DOTALL)  # Match anything between the first and last curly braces
+    if match:
+        json_str = match.group(0)  # Extract the matched JSON string
+        return json_str.strip()  # Remove leading/trailing spaces
+    else:
+        raise ValueError("No valid JSON found in the response.")
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT"))
